5cebde660e2f513bffcb27f798b9302717ed5f19
matthew <shaggysci3@gmail.com>
2023-09-27 07:59:37 -0400

    bsLab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+newHeader.id = "victory";
+
+const body = document.querySelector('body'); 
+body.appendChild(newHeader); 
+
+
